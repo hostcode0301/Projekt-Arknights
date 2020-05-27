b2d290d74c7c93f9d7a9de8d63146711af287094
Tran Thien Phu <hostcode0301@gmail.com>
2020-05-27 19:57:59 +0700

    Upload Projekt

---
+++ app.js
@@ -0,0 +1,45 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const Rhodes = document.querySelector(".Rhodes");
+const headline = document.querySelector(".headline");
+
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero,1, {height: "0%"}, {height: "80%", ease:Power2.easeInOut})
+    .fromTo(hero, 1.2, { width: "100%" }, { width: "80%", ease: Power2.easeInOut})
+    .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut}, "-=1.2")
+    .fromTo(logo, 0.5, {opacity: 0, x: 30},{opacity: 1, x: 0}, "-=0.5")
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle("open");
+})
+
+let ExiaE2 = document.querySelector(".ExiaE2");
+
+function Exia_Hide(x) {
+    x.style.display = "none";
+    ExiaE2.style.display = "block";
+}
+
+function Exia_Show(x) {
+    x.style.display = "block";
+    ExiaE2.style.display = "none";
+}
+
+let SariaE2 = document.querySelector(".SariaE2");
+
+function Saria_Hide(y) {
+    y.style.display = "none";
+    SariaE2.style.display = "block";
+}
+
+function Saria_Show(y) {
+    y.style.display = "block";
+    SariaE2.style.display = "none";
+}
